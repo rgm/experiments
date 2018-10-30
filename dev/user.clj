a7ae5682baa03baea8f59263048139ea02fefeea
Ryan McCuaig <ryan@ryanmccuaig.net>
2018-10-29 18:42:36 -0700

    Tidies up dev configs for starting up processes

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,6 @@
 (ns user
-  (:require [sample.helpers :as help]))
+  (:require [figwheel.main.api]
+            [sample.helpers :as help]))
 
 ;;; starting up figwheel manually so that we can piggieback into the browser
 ;;; from cljs files
@@ -10,7 +11,18 @@
 ;;; vscode + calva
 ;;; TODO - where's the doc for this??
 
+(def nrepl-port 7888)
+(def figwheel-build-id "config/dev")
+
 (defn go
   []
   (help/start-nrepl-server! 7888)
-  (help/start-figwheel-build! "config/dev"))
+  (help/start-figwheel-build! figwheel-build-id))
+
+(defn cljs-repl
+  []
+  (figwheel.main.api/cljs-repl figwheel-build-id))
+
+(defn repl-env
+  []
+  (figwheel.main.api/repl-env figwheel-build-id))
