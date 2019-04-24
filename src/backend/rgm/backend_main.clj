9e0abfdce6a7b0e96f5863febcdf29db56bb1478
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-04-24 09:36:10 -0700

    Starts using aero, pull nrepl into its own namespace

--- src/backend/rgm/backend_main.clj
+++ src/backend/rgm/backend_main.clj
@@ -1,45 +1,27 @@
 (ns rgm.backend-main
+  "cli, etc for starting all server-side processes"
+  (:gen-class)
   (:require
     [aero.core :as aero]
-    [cider.nrepl]
-    [cider.piggieback]
-    [clojure.java.io :as io]
-    [figwheel.main.api]
     [integrant.core :as ig]
-    [nrepl.server :as nrepl]
-    [rgm.http]
     [taoensso.timbre :as timbre]))
 
-(defn make-nrepl-handler []
-  (let [middleware-symbols (conj cider.nrepl/cider-middleware
-                                 'cider.piggieback/wrap-cljs-repl)
-        middleware-vars (map resolve middleware-symbols)]
-  (apply nrepl/default-handler middleware-vars)))
-
-(defmethod ig/init-key :rgm/nrepl
-  [_ {:keys [port] :or {port 0}}] ;; nrepl picks randomly if 0
-  (let [server (nrepl/start-server
-                 :port port
-                 :handler (make-nrepl-handler))
-        port (:port server)]
-    (timbre/info "starting nrepl server on port" port)
-    (spit ".nrepl-port" port)
-    server))
-
-(defmethod ig/halt-key! :rgm/nrepl
-  [_ server]
-  (timbre/info "stopping nrepl server")
-  (nrepl/stop-server server)
-  (io/delete-file ".nrepl-port" true))
-
-(defonce running-system (atom nil))
+;; register integrant components
+(require
+  '[rgm.http]
+  '[rgm.nrepl])
 
 (defn make-ig-system
-  []
-  {:rgm/nrepl {:port 0}
-   :rgm/http {:port 8080}})
+  "reads an external edn config, resolves all aero and ig refs"
+  ([]
+   (make-ig-system "config.edn"))
+  ([config-filename]
+   (ig/prep (aero/read-config config-filename))))
 
 (defn -main
   []
   (timbre/set-level! :info)
   (ig/init (make-ig-system)))
+
+(comment
+  (make-ig-system))
