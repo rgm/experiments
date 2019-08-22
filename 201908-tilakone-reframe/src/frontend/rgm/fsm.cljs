36c670c6f69c2ddd71c2e47907d5a596f68dbedb
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-08-22 13:01:16 -0400

    More sketching

--- 201908-tilakone-reframe/src/frontend/rgm/fsm.cljs
+++ 201908-tilakone-reframe/src/frontend/rgm/fsm.cljs
@@ -1,13 +1,51 @@
 (ns rgm.fsm
   "FSM example stitching tilakone together with re-frame"
   (:require
-   [re-frame.core :as re-frame :refer [dispatch subscribe]]
+   [clojure.pprint :as pp]
+   [re-frame.core :as re-frame :refer [dispatch subscribe reg-event-fx reg-sub]]
    [tilakone.core :as tk]))
 
 (def box
   {:border "1px solid #999"
    :padding "1em"
    :margin "1em"})
+
+(defn simple-fsm
+  ([]
+   (simple-fsm :state/start))
+  ([initial-state]
+  {::tk/state initial-state
+   ::tk/states [{::tk/name :state/start}
+                {::tk/name :state/added-to-seed}
+                {::tk/name :state/compliant}]}))
+
+(defn compliance-fsm
+  ([]
+   (compliance-fsm :state/start))
+  ([initial-state]
+  {::tk/state initial-state
+   ::tk/states [{::tk/name :state/start}
+                {::tk/name :state/added-to-seed}
+                {::tk/name :state/waiting-for-data}
+                {::tk/name :state/checking-data}
+                {::tk/name :state/data-quality-good}
+                {::tk/name :state/data-quality-bad}
+                {::tk/name :state/compliant}
+                {::tk/name :state/exempt}
+                {::tk/name :state/publicly-disclosed}]}))
+
+(reg-event-fx
+ ::init
+ (fn [{db :db} _]
+   {:db (-> db
+            (assoc ::simple (simple-fsm))
+            (assoc ::compliance (compliance-fsm)))}))
+
+(reg-sub
+ ::debug-fsms
+ (fn [db _]
+   {::simple (::simple db)
+    ::compliance (::compliance db)}))
 
 (defn CycleSummary
   [k]
@@ -18,10 +56,12 @@
 
 (defn Layout
   []
-  (let []
+  (let [*debug (subscribe [::debug-fsms])]
     [:<>
      [:h1 "fsm examples"]
-     [CycleSummary :simple]
-     [CycleSummary :complex]
-     [:button {:on-click #(dispatch [::init])} "reset"]]))
-
+     [CycleSummary ::simple]
+     [CycleSummary ::compliance]
+     [:button {:on-click #(dispatch [::init])} "init fsms"]
+     [:details {:open true}
+      [:summary "fsms"]
+      [:pre [:code (with-out-str (pp/pprint @*debug))]]]]))
