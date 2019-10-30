59dc0fa90238d319272328610033ea9e6a4795a4
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-10-30 14:17:36 -0700

    Adds reitit dep to stress-test the classloader
    Seems there are some clj libs that have internal java classes that mess
    up this packaging system.

--- 201910-skinny-aot/src/rgm/core.clj
+++ 201910-skinny-aot/src/rgm/core.clj
@@ -1,7 +1,15 @@
 (ns rgm.core
   (:gen-class)
-  (:require [taoensso.timbre :as timbre]))
+  (:require
+   [reitit.core :as r]
+   [taoensso.timbre :as timbre]))
+
+(def router
+  (r/router
+    [["/api/ping" ::ping]
+     ["/api/orders/:id" ::order]]))
 
 (defn -main
   []
+  (timbre/info "reitit router is" router) ;; should cause a classloader bail-out
   (timbre/info "yep, we booted right, from a skinny jar"))
