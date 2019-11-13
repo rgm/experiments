c5af7bf7dca14c44ae9c8cc5350d1fcd45b2ef1e
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-11-13 12:18:27 -0800

    Removes pack, badigeon

--- 201910-skinny-aot/dev/rgm/build.clj
+++ 201910-skinny-aot/dev/rgm/build.clj
@@ -1,41 +1,5 @@
-(ns rgm.build
-  (:require
-   [mach.pack.alpha.skinny]
-   [badigeon.bundle :refer [bundle make-out-path]]
-   [badigeon.clean]
-   [badigeon.compile :as c]
-   [badigeon.jar]
-   [badigeon.pom]
-   ))
+(ns rgm.build)
 
 (defn -main
   []
-  (compile 'rgm.core)
-  #_(prn "foo"))
-
-(comment
-
- (compile 'rgm.core)
-
- (badigeon.clean/clean "target")
-
- (bundle "target" {:excluded-libs ['rgm.core]})
- (bundle (make-out-path 'lib nil))
- (c/compile 'rgm.core {:compile-path "target/classes"})
-
- (badigeon.bundle/bin-script "." 'rgm.core)
-
- (spit "test-manifest.txt" (badigeon.jar/make-manifest 'rgm.core {"Built-By" "ryan@ryanmccuaig.net"}))
-
- (badigeon.pom/sync-pom 'rgm.foo)
-
-
- (badigeon.jar/jar 'rgm.foo
-                   {:mvn/version "1.0.1.rc1"}
-                   {:paths ["targets/classes"]
-                    :mvn/repos '{"clojars" {:url "https://repo.clojars.org/"}}
-                    :manifest {"Built-By" "ryan@ryanmccuaig.net"}
-                    :allow-all-dependencies? true
-                    :main 'rgm.core})
-
- (-main))
+  (compile 'rgm.core))
