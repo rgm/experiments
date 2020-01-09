976e9458157608cf2b597af890ff50bec2767d7d
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-01-09 15:12:22 -0800

    WIP

--- 201911-juxt-pull/src/user.clj
+++ 201911-juxt-pull/src/user.clj
@@ -32,6 +32,18 @@
 
 (def pull2 (partial juxt.pull.core/pull more-data))
 
+(def om-data
+  {:properties/by-id {1 #:property{:id 1 :address "123 main"}
+                      2 #:property{:id 2 :address "456 elm"}}
+   :cycles/by-id {:cycle-a #:cycle{:id 2018
+                                   :title "2018 year"
+                                   :properties [[:property/id 1]]
+                                   }
+                  :cycle-b #:cycle{:id 2019
+                                   :title "2019 year"
+                                   :properties [[:property/id 1]
+                                                [:property/id 2]]}}})
+
 (comment
  
 
