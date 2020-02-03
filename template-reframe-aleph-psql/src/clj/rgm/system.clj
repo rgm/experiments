3d4b3d35967ed32e20d50bdb99fe34bca6553dd3
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-02-03 14:11:15 -0800

    Moves config to edn + aero, uses env vars

--- template-reframe-aleph-psql/src/clj/rgm/system.clj
+++ template-reframe-aleph-psql/src/clj/rgm/system.clj
@@ -1,13 +1,12 @@
 (ns rgm.system
   (:require
-   ; [juxt.aero]
-   [juxt.clip.core :as clip]))
+   [aero.core]
+   [clojure.edn]
+   [clojure.java.io :as io]
+   [juxt.clip.core]))
 
 (defn system-config
   "Generate a system map"
   [profile]
-  {:components
-   {:system-profile {:start `(identity ~profile)}
-    :route-tree     {:start `(rgm.http-server/route-tree (clip/ref :system-profile))}
-    :ring-handler   {:start `(rgm.http-server/make-app (clip/ref :route-tree))}
-    :http-server    {:start `(aleph.http/start-server (clip/ref :ring-handler) {:port 8080})}}})
+  {:components (aero.core/read-config (io/resource "system.edn")
+                                      {:profile profile})})
