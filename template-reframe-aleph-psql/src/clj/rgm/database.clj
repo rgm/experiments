c0b2f19e5ef500e1f34f61514a8244e63ca227de
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-02-05 08:39:03 -0800

    Switches ragtime -> migratus
    Migratus doesn't have as much trouble locating migrations within a jar
    file, plus has some repl helper fns to actually create the migrations.

--- template-reframe-aleph-psql/src/clj/rgm/database.clj
+++ template-reframe-aleph-psql/src/clj/rgm/database.clj
@@ -1,28 +1,25 @@
 (ns rgm.database
   (:require
-   [ragtime.jdbc]
-   [ragtime.repl]
+   [migratus.core :as migratus]
    [rgm.system]
    [taoensso.timbre :as timbre]))
 
-(defn make-ragtime-config
+(defn make-migratus-config
   [db-spec]
-  {:datastore  (ragtime.jdbc/sql-database {:connection-uri db-spec})
-   :migrations (ragtime.jdbc/load-resources "migrations")
-   :reporter   (fn [_store op id]
-                 (timbre/info (case op :up "applying" :down "rolling back")
-                              "migration" id))})
+  {:store         :database
+   :migration-dir "migrations/"
+   :db            db-spec})
 
 (defn migrate-to-latest
   "Migrate the profile db to apply all new migrations."
   [profile]
   (if-let [db-spec (rgm.system/db-spec (rgm.system/config profile))]
-    (ragtime.repl/migrate (make-ragtime-config db-spec))
+    (migratus/migrate (make-migratus-config db-spec))
     (timbre/error "no db-spec found in profile" profile)))
 
 (defn rollback-latest
   "Roll back the newest migration in the profile db."
   [profile]
   (if-let [db-spec (rgm.system/db-spec (rgm.system/config profile))]
-    (ragtime.repl/rollback (make-ragtime-config db-spec))
+    (migratus/rollback (make-migratus-config db-spec))
     (timbre/error "no db-spec found in profile" profile)))
