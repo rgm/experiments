8ad8e44e8491e44c988374c2a78132e5c0159dca
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-05-23 00:42:56 +0000

    I think it might be working

--- 202006-pso/src/pso.clj
+++ 202006-pso/src/pso.clj
@@ -7,9 +7,7 @@
    [taoensso.timbre :as timbre]
    [taoensso.tufte :as tufte :refer [profile p]]))
 
-(tufte/add-basic-println-handler! {})
-
-;; * Test fn
+;; * Test fns
 
 ;; z = x * exp(-(x^2 + y^2)) with known min at (-0.707, 0, -0.42)
 ;; https://www.youtube.com/watch?v=ckbdXbbNNNA
@@ -20,6 +18,16 @@
 
 (comment
   (z KNOWN-OPTIMUM) ;; => -0.4288819424803531
+  (z {:x 1 :y 2})
+  (z {:x ##Inf :y ##Inf})
+  )
+
+(defn parabowl
+  [{:keys [x y]}]
+  (+ (* x x) (* y y)))
+
+(comment
+  (parabowl {:x 1 :y 1})
   )
 
 ;; * Particles and swarms
@@ -35,10 +43,9 @@
                 :best-pos         empty-pos}))
 
 (defn make-swarm
+  "A swarm is just a coll of particles."
   [empty-pos n]
   (repeatedly n #(make-particle empty-pos)))
-
-(def *swarm-best-position (atom {:x 0 :y 0}))
 
 ;; * vector arithmetic
 
@@ -62,7 +69,6 @@
         (v* (* r2 cognitive-coeff) (v- best-pos current-pos))
         (v* (* r3 social-coeff)    (v- best-swarm-pos current-pos)))))
 
-
 (defn get-next-position
   [pos velocity]
   (v+ pos velocity))
@@ -78,25 +84,68 @@
                 best-pos)))
           best-pos particles))
 
+(defn update-velocity
+  [inertial-coeff cognitive-coeff social-coeff best-swarm-pos
+   particle]
+  (let [next-velocity (get-next-velocity
+                       inertial-coeff cognitive-coeff social-coeff
+                       best-swarm-pos particle)]
+    (assoc particle :current-velocity next-velocity)))
+
+(defn update-position
+  [particle]
+  (let [velocity (:current-velocity particle)]
+    (update particle :current-pos (partial v+ velocity))))
+
+(defn update-best-position
+  [arbiter f particle]
+  (let [best-cost (f (:best-pos particle))
+        current-cost (f (:current-pos particle))]
+    (if (= current-cost (arbiter current-cost best-cost))
+      (assoc particle :best-pos (:current-pos particle))
+      particle)))
+
+(defn spy
+  [note x]
+  ; (prn note x)
+  x)
+
 (defn advance-swarm
+  "Update velocities, then positions, then designate a new best."
   [inertial-coeff cognitive-coeff social-coeff best-swarm-pos
    arbiter f
    swarm]
-  swarm)
-
-(defn parabowl
-  [{:keys [x y]}]
-  (+ (* x x) (* y y)))
+  (->> swarm
+       (spy "swarm")
+       (map #(update-velocity inertial-coeff cognitive-coeff social-coeff
+                              best-swarm-pos
+                              %))
+       (spy "vel")
+       (map update-position)
+       (spy "pos")
+       (map #(update-best-position arbiter f
+                                   %))
+       (spy "best")
+       )
+  #_(map (comp (partial update-best-position arbiter f)
+             update-position
+             (partial update-velocity inertial-coeff cognitive-coeff social-coeff
+                      best-swarm-pos))
+       swarm))
 
 (defn constant-inertia
-  []
+  [& _]
   0.9)
 
 (defn declining-inertia
-  "in [0, ]. Make big for better exploration. Can reduce linearly over epochs
+  "Make big for better exploration. Can reduce linearly over epochs
    0.9 -> 0.2 for convergence."
   [max-epochs epoch]
-  0.9)
+  (let [maximum 0.9
+        minimum 0.2
+        domain (- maximum minimum)
+        progress (/ epoch max-epochs)]
+    (+ minimum (* (- 1 progress) domain))))
 
 (defn safe-min
   "JIC the infinity start plus objective fn makes things go nuts."
@@ -105,11 +154,6 @@
     (Double/isNaN a) b
     (Double/isNaN b) a
     :else (min a b)))
-
-(comment
-  (z {:x 1 :y 2})
-  (z {:x ##Inf :y ##Inf})
-  )
 
 (defn find-optimum
   "
@@ -121,7 +165,7 @@
    :arbiter           pso/safe-min ;; arbiter fn returns the 'best' from 2+ positions; aka. mappend
    :empty-pos         {:x ##Inf :y ##Inf}} ;; aka. mempty
 
-  (huh, arbiter + empty-pos turn out to be a monoid).
+   (huh, arbiter + empty-pos turn out to be a monoid).
    https://youtu.be/JhgDMAm-imI?t=777"
   [params f]
   (let [{:keys [cognitive-coeff social-coeff inertial-coeff-fn
@@ -132,10 +176,12 @@
       (if (<= current-epoch 0)
         best-swarm-pos
         (let [inertial-coeff (inertial-coeff-fn epochs current-epoch)
-              swarm' (advance-swarm inertial-coeff cognitive-coeff social-coeff
-                                    arbiter f
-                                    best-swarm-pos
-                                    swarm)
-              best-swarm-pos' (get-best-position arbiter f best-swarm-pos swarm')]
+              swarm' (p ::advance-swarm
+                        (advance-swarm inertial-coeff cognitive-coeff social-coeff
+                                       best-swarm-pos
+                                       arbiter f
+                                       swarm))
+              best-swarm-pos'(p ::get-best-position
+                                (get-best-position arbiter f best-swarm-pos swarm'))]
           (timbre/debug current-epoch best-swarm-pos')
           (recur (dec current-epoch) swarm' best-swarm-pos'))))))
--- 202006-pso/src/pso_test.clj
+++ 202006-pso/src/pso_test.clj
@@ -3,7 +3,10 @@
    [pso]
    [clojure.test :as t :refer [deftest testing is]]
    [taoensso.timbre :as timbre]
-   [taoensso.tufte :as tufte :refer [profile]]))
+   [taoensso.tufte :as tufte :refer [p profile]]))
+
+(tufte/add-basic-println-handler! {})
+(timbre/set-level! :warn)
 
 (deftest get-next-velocity-test
   (testing "velocity+inertia only"
@@ -31,6 +34,15 @@
                    (pso/get-next-velocity 0 0 1 best-swarm-pos particle))]
       (is (= {:x 3 :y 3} actual))))
   (testing "all three together"
+    ;; 2,2 =inertia=> 0.5,0.5 =cognitive=> 0.5,1 =social=> 1,1
+    (let [particle {:current-pos      {:x 2 :y 2}
+                    :current-velocity {:x -1 :y -1}
+                    :best-pos         {:x 2 :y 1}}
+          best-swarm-pos {:x 1 :y 2}
+          actual (binding [pso/*make-random* (constantly 1)]
+                   (pso/get-next-velocity 1 1 1 best-swarm-pos particle))]
+      (is (= {:x -2 :y -2} actual))))
+  (testing "all three together"
     ;; 0,0 =inertia=> 0.5,0.5 =cognitive=> 0.5,1 =social=> 1,1
     (let [particle {:current-pos      {:x 0 :y 0}
                     :current-velocity {:x 1 :y 1}
@@ -41,25 +53,22 @@
       (is (= {:x 1.0 :y 1.0} actual)))))
 
 (deftest advance-swarm-test
-  (binding [pso/*make-random* (constantly 1)]
-    (let [
-          arbiter             min
-          f                   pso/parabowl
-          starting-swarm-best {:x 0.5 :y 0}
-          swarm               [{:current-pos {:x 1 :y 1}
-                                :current-velocity {:x -0.25 :y -0.25}
-                                :best-pos {:x ##Inf :y ##Inf}}]
-          best-swarm-pos      (pso/get-best-position arbiter f
-                                                 starting-swarm-best
-                                                 swarm)
-          expected            [{:current-pos {:x 0.75 :y 0.75}
-                                :current-velocity {:x 1 :y 2}
-                                :best-pos {:x 1 :y 1}}]
-          actual              (pso/advance-swarm 1 1 1
-                                             best-swarm-pos
-                                             arbiter f
-                                             swarm)]
-      (is (= expected actual)))))
+  ()
+  (let [arbiter             min
+        f                   pso/parabowl
+        swarm               [{:current-pos      {:x  2 :y  2}
+                              :current-velocity {:x -1 :y -1}
+                              :best-pos         {:x  2 :y  1}}]
+        best-swarm-pos                          {:x  1 :y  2}
+        expected            [{:current-pos      {:x  0 :y  0}
+                              :current-velocity {:x -2 :y -2}
+                              :best-pos         {:x  0 :y  0}}]
+        actual              (binding [pso/*make-random* (constantly 1)]
+                              (pso/advance-swarm 1 1 1
+                                                 best-swarm-pos
+                                                 arbiter f
+                                                 swarm))]
+    (is (= expected actual))))
 
 (deftest safe-min-test
   (is (= 2 (pso/safe-min ##NaN 2)))
@@ -67,7 +76,6 @@
   (is (= ##NaN (pso/safe-min ##NaN ##NaN))))
 
 (deftest get-best-position-test
-  (timbre/set-level! :warn)
   (let [f (fn [{:keys [x y]}] (+ x y))
         particles [{:current-pos {:x 0.23, :y 0.53}}
                    {:current-pos {:x 0.17, :y 0.11}}
@@ -81,21 +89,33 @@
     (is (= {:x 0.17, :y 0.11}
            (pso/get-best-position pso/safe-min f {:x ##NaN :y ##NaN} particles)))))
 
+(defn equalish [a b] (< (- (Math/abs a) (Math/abs b)) 1e-4))
+
+(defn position-equalish [a b] (and (timbre/spy :info (equalish (:x a) (:x b))) (timbre/spy :info (equalish (:y a) (:y b)))))
+
+(deftest declining-inertia-test
+  (is (equalish 0.9  (pso/declining-inertia 10 0)))
+  (is (equalish 0.55 (pso/declining-inertia 10 5)))
+  (is (equalish 0.2  (pso/declining-inertia 10 10))))
+
 (deftest find-optimum-test
-  (timbre/set-level! :warn)
-  (let [search-params {:epochs            3
-                       :n-particles       3 ;; 2x the variables?
-                       :cognitive-coeff   2 ;; attraction to particle's own best memory
-                       :social-coeff      2 ;; exchange of info about swarm best between particles
+  (let [search-params {:epochs            100
+                       :n-particles       1000
+                       :cognitive-coeff   2
+                       :social-coeff      2
                        :inertial-coeff-fn pso/declining-inertia
-                       :arbiter           pso/safe-min ;; arbiter fn returns the 'best' from 2+ positions; aka. mappend
-                       :empty-pos         {:x ##Inf :y ##Inf}} ;; aka. mempty
-        function (fn [{:keys [x y] :as pos}] (timbre/info pos) (* x (Math/exp (- (+ (* x x) (* y y))))))]
-    (is (= {:x -0.7071068, :y 0.0}
-           (profile {} (pso/find-optimum search-params function))))))
+                       :arbiter           pso/safe-min
+                       :empty-pos         {:x 1 :y 1}}
+        cost-surface  (fn [{:keys [x y]}]
+                        ;; in practice, MEMOIZE THIS
+                        (p ::cost-surface
+                           (* x (Math/exp (- (+ (* x x) (* y y)))))))
+        expected      {:x -0.7071068, :y 0.0}
+        actual        (profile {} (pso/find-optimum search-params
+                                                    cost-surface))]
+    (is (position-equalish expected actual))))
 
 (comment
   (pso/v+ {:x 1 :y 2} {:x 3 :y 4})
   (pso/v- {:x 1 :y 1} {:x 1 :y 1})
   (pso/v* 12 {:x 1 :y 12}))
-
