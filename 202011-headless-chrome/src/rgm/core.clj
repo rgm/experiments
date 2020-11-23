ad20991478080616f21cf1f81e995303ca1fb758
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-11-23 18:07:51 +0000

    Save out pdf file as binary
    Per https://github.com/tatut/clj-chrome-devtools/issues/29#issuecomment-732234883

--- 202011-headless-chrome/src/rgm/core.clj
+++ 202011-headless-chrome/src/rgm/core.clj
@@ -4,14 +4,15 @@
    [clj-chrome-devtools.commands.page   :as page]
    [clj-chrome-devtools.core            :as chrome]
    [clj-chrome-devtools.impl.connection :as chrome.conn]
-   #_[clj-chrome-devtools.commands.dom    :as dom])
+   [clojure.java.io                     :as io]
+   #_[clj-chrome-devtools.commands.dom  :as dom])
   (:import
    [java.util Base64]))
 
 (defn b64-decode
   "https://stackoverflow.com/a/39188819/53790"
   [to-decode]
-  (String. (.decode (Base64/getDecoder) to-decode)))
+  (.decode (Base64/getDecoder) to-decode))
 
 (comment
   (def ws-client (chrome.conn/make-ws-client :max-msg-size-mb (* 1024 1024 256)))
@@ -20,7 +21,8 @@
   (page/navigate c {:url "https://github.com"})
   (def resp (page/print-to-pdf c {:page-ranges "1-3"}))
   (def pdf-data (b64-decode (:data resp)))
-  (spit "github.pdf" pdf-data))
+  (with-open [out (io/output-stream "github.pdf")]
+    (.write out pdf-data)))
 
 (defn -main
   []
@@ -30,5 +32,6 @@
         _         (auto/to a "https://github.com")
         resp      (page/print-to-pdf c {:page-ranges "1-2"})
         pdf-data  (b64-decode (:data resp))]
-    (spit "github-test.pdf" pdf-data)
+    (with-open [out (io/output-stream "github-test.pdf")]
+      (.write out pdf-data))
     (System/exit 0)))
