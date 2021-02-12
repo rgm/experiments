708a4ef253d41a8f6764ebaaff8a20a1dfaa7c9f
Ryan McCuaig <ryan@ryanmccuaig.net>
2021-02-12 23:03:16 +0000

    Set up sorting

--- 202102-react-table/src/main/my/app.cljs
+++ 202102-react-table/src/main/my/app.cljs
@@ -3,44 +3,63 @@
    features."
   (:require
    [cljs-bean.core :refer [bean ->clj ->js]]
-   [react]
-   [react-table :refer [useTable]]
+   [react :refer [useMemo]]
+   [react-table :refer [useTable useSortBy useFilters]]
    [reagent.core :as rg]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [taoensso.timbre :as timbre]))
 
 ;; https://github.com/reagent-project/reagent/blob/master/doc/ReactFeatures.md#hooks
 ;; https://react-table.tanstack.com/docs/quick-start
 
 ;; * Table {{{1
 
+(defn Dim [[mag unit]] [:<>
+                        [:div {:class ["red-500"]} mag]
+                        [:div {:class ""} (name unit)]])
 
-(def data [{:subref "A" :spacing [16 :in] :xmit [0.05 :W_m2k]}
-           {:subref "B" :spacing [16 :in] :xmit [0.05 :W_m2k]}
-           {:subref "C" :spacing [16 :in] :xmit [0.05 :W_m2k]}])
+(def data [{:subref  "A"
+            :spacing [:>f Dim [16 :in]]
+            :xmit    [Dim [0.25 :W_m2k]]}
+           {:subref  "B"
+            :spacing [:>f Dim [32 :in]]
+            :xmit    [Dim [0.05 :W_m2k]]}
+           {:subref  "C"
+            :spacing [:>f Dim [48 :in]]
+            :xmit    [Dim [0.15 :W_m2k]]}])
 
-(def columns [{:Header "Subref" :accessor :subref}
-              {:Header "Spacing" :accessor :spacing}
-              {:Header "Transmittance" :accessor :xmit}])
+(def columns [{:Header "Subref" :accessor :subref :sortType "basic"}
+              {:Header "Spacing" :accessor :spacing :sortType "basic"}
+              {:Header "Transmittance" :accessor :xmit :sortType "basic"}])
 
 (defn Table []
-  (let [instance (useTable (->js {:columns columns :data data}))
+  (let [memoized-columns (useMemo #(->js columns) #js [])
+        memoized-data    (useMemo #(->js data) #js [])
+        instance (useTable (->js {:columns memoized-columns
+                                  :data    memoized-data})
+                           useSortBy)
         {:keys [getTableProps getTableBodyProps
                 headerGroups rows prepareRow]} (bean instance)]
     (tap> instance)
-    [:table (merge (->clj (getTableProps)) {})
-     (into [:thead]
+    [:table (merge (->clj (getTableProps))
+                   {:class "border"})
+     (into [:thead {:class "border-b"}]
            (for [hg headerGroups
                  :let [{:keys [getHeaderGroupProps headers]} (bean hg)]]
              (into [:tr (merge (->clj (getHeaderGroupProps)) {})]
                    (for [col headers
-                         :let [{:keys [getHeaderProps render]} (bean col)]]
-                     [:th (merge (->clj (getHeaderProps col)) {})
-                      (render "Header")]))))
+                         :let [{:keys [getHeaderProps getSortByToggleProps
+                                       render isSorted isSortedDesc]} (bean col)]]
+                     [:th (merge (->clj (getHeaderProps (getSortByToggleProps)))
+                                 {})
+                      (render "Header")
+                      [:span (if isSorted (if isSortedDesc "↓" "↑") "-")]]))))
      (into [:tbody (merge (->clj (getTableBodyProps)) {})]
            (for [row rows
                  :let [_ (prepareRow row)
                        {:keys [getRowProps cells]} (bean row)]]
-             (into [:tr (merge (->clj (getRowProps)) {})]
+             (into [:tr (merge (->clj (getRowProps))
+                               {:class ["hover:bg-blue-300" "transition"]})]
                    (for [cell cells
                          :let [{:keys [getCellProps render]} (bean cell)]]
                      [:td (merge (->clj (getCellProps)) {})
