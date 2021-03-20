fa7cfcd5677e99a95148da51b567f7ea02b47474
Ryan McCuaig <ryan@ryanmccuaig.net>
2021-03-20 15:54:15 +0000

    Add some vega to test page

--- 202102-headless-chrome/src/rgm/app.cljs
+++ 202102-headless-chrome/src/rgm/app.cljs
@@ -2,11 +2,52 @@
   "Entry point for single-page app."
   (:require
    [reagent.dom :as rdom]
-   [reagent.core :as rg]))
+   [reagent.core :as rg]
+   [rgm.vega :as vega]))
+
+(defn Vega:did-mount [a-ref this-component]
+  (let [props     (rg/props this-component)
+        vega-spec (clj->js (:spec props))
+        runtime   (.parse js/vega vega-spec)
+        view      (-> (js/vega.View. runtime)
+                      (.initialize a-ref)
+                      (.renderer "svg")
+                      (.hover))]
+    (.logLevel view js/vega.Warn)
+    (.runAsync view)
+    view))
+
+(defn Vega:did-update [^js/vega.View vega-view this-component]
+  (let [props (rg/props this-component)
+        {:keys [debug]} props]
+    (.runAsync vega-view)
+    ;; debug-log requested datasets
+    (doseq [dataset-name debug]
+      (js/console.debug dataset-name (.data vega-view dataset-name)))))
+
+(defn VegaRenderer [_props]
+  (let [*a-ref     (atom nil)
+        *vega-view (atom nil)]
+    (rg/create-class
+     {:component-did-mount
+      (fn did-mount [this] (let [view (Vega:did-mount @*a-ref this)]
+                             (reset! *vega-view view)
+                             ;; give it one initial bump, in case of static data
+                             (Vega:did-update @*vega-view this)))
+
+      :component-did-update
+      (fn did-update [this _old-argv] (Vega:did-update @*vega-view this))
+
+      :reagent-render
+      (fn render [_props]
+        [:div {:class "w-full h-full" :ref #(reset! *a-ref %)}])})))
 
 (defn Root []
   [:div
-   [:h1 {:class ""} "hi from within react"]])
+   [:h1 {:class "text-red-500"} "hi from within react with some styling"]
+   [:div {:class "mt-10 w-1/2 h-64 shadow p-3"}
+    (let [vega-spec (vega/bar-chart vega/bar-data)]
+      [VegaRenderer {:spec vega-spec :debug ["table"]}])]])
 
 (defn ^:dev/before-load stop [])
 
