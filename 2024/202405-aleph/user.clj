fd67525f1016e80822d547c6da3705a29744af17
Ryan McCuaig <ryan@ryanmccuaig.net>
2024-05-03 23:13:15 +0000

    Make header stripper work

--- 2024/202405-aleph/user.clj
+++ 2024/202405-aleph/user.clj
@@ -4,6 +4,7 @@
    https://github.com/clj-commons/aleph/issues/512#issuecomment-695972882"
   (:require
    [aleph.http]
+   [aleph.netty]
    [clojure.test :as t :refer [deftest is]])
   (:import
    (io.netty.channel ChannelOutboundHandlerAdapter ChannelPipeline)
@@ -11,19 +12,44 @@
 
 (def HANDLER (constantly {:status 200}))
 
-(def SCRUBBER
+(defn make-scrubber []
   (proxy [ChannelOutboundHandlerAdapter] []
     (write [ctx msg chan-promise]
-      (prn "running write method")
+      (println "--- RUNNING WRITE METHOD")
+      (prn "-- ctx:" ctx)
+      (prn "-- msg:" msg)
+      (prn "-- chan-promise: " chan-promise)
       (when (instance? HttpResponse msg)
         (prn "removing header")
         (.remove (.headers msg) "Server"))
-      (proxy-super ctx msg chan-promise))))
+      (proxy-super write ctx msg chan-promise))))
+
+(defn make-scrubber-2 []
+  ;; thx @oyakushev
+  ;; https://clojurians.slack.com/archives/C0G922PCH/p1714776636228319?thread_ts=1714774471.146039&cid=C0G922PCH
+
+  ;; don't just instantiate one of these with def and pass it on; you seem to
+  ;; need to hand over a new one fairly often, hence the defn
+  (aleph.netty/channel-outbound-handler
+   :write ([_ ctx msg chan-promise]
+           (prn "running write method")
+           (println "--- RUNNING WRITE METHOD")
+           (prn "-- ctx:" ctx)
+           (prn "-- msg:" msg)
+           (prn "-- chan-promise: " chan-promise)
+           (when (instance? HttpResponse msg)
+             (prn "removing header")
+             (.remove (.headers msg) "Server"))
+           (.write ctx msg chan-promise))))
 
 (def XF (fn [^ChannelPipeline pipeline]
-          (prn "transforming pipeline")
+          (println "--- TRANSFORMING PIPELINE")
+          (prn "before" (.names pipeline))
           (doto pipeline
-            (.addLast "scrubber" SCRUBBER))))
+            ;; couldn't tell you why it needs to be here but it does ¯\_(ツ)_/¯
+            ;; .addFirst, .addLast, etc. don't work
+            (.addBefore "request-handler" "scrubber" (make-scrubber-2)))
+          (prn "after" (.names pipeline))))
 
 (def OPTS {:port 9999 :pipeline-transform XF})
 
@@ -45,5 +71,5 @@
 
 (comment
   (def server (aleph.http/start-server HANDLER OPTS))
-  (deref (aleph.http/get ENDPOINT))
+  (get @(aleph.http/get ENDPOINT) :headers)
   (.close ^java.io.Closeable server))
