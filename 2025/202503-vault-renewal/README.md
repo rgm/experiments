0c9bb4e2c7fe530b06b06d2ad6dff50452a33fc5
Ryan McCuaig <ryan@ryanmccuaig.net>
2025-03-03 22:52:54 +0000

    Finish vault experiment

--- 2025/202503-vault-renewal/README.md
+++ 2025/202503-vault-renewal/README.md
@@ -1,6 +1,16 @@
 # amperity vault token renewal
 
-Exploring how to make amperity/vault-clj renew periodic tokens properly.
+Exploring how to make amperity/vault-clj renew periodic tokens properly. This
+project issues a 2m TTL renewable token, and makes sure that it always gets
+renewed with about 45-50s to go.
+
+The trick seems to be that we need to supply `:vault.auth/renewable? true` and
+an instant value for `:vault.auth/expires-at` in the auth info supplied to the
+vault client. If these keys aren't present, then the renewal machinery will not
+kick in. 
+
+Note that `vault.auth.token/resolve-token!` will annotate the auth state
+correctly by just asking vault, but this isn't automatic.
 
 To run:
 
@@ -13,5 +23,6 @@ clj user.clj
 ```
 
 Expected:
-- in clj repl, should be able to list secrets after 1 min
+- in clj repl, should still be able to list secrets after 1 min
 - in overmind log, should see the token renew, not get kicked out after 1min
+- if I run the dotimes, the TTL countdown will jump back to 120s at some point
--- 2025/202503-vault-renewal/user.clj
+++ 2025/202503-vault-renewal/user.clj
@@ -1,27 +1,52 @@
 (ns user
   (:require
+   [clojure.string :as str]
    [vault.client :as vault]
    [vault.secret.kv.v2 :as kv]
-   [vault.auth.token :as tok]))
+   [vault.auth.token :as tok]
+   [vault.util :as u]))
 
 (defonce client nil)
 
 (defn vault-addr [] (System/getenv "VAULT_ADDR"))
 
-(defn read-token [] (slurp "CLIENT_TOKEN.txt"))
+(defn read-token []
+  (str/trim (slurp "CLIENT_TOKEN.txt")))
+
+(defn read-auth []
+  ;; we don't get renewal behaviour unless we supply renewable true
+  ;; and an expiry instant
+  ;; OR ... call vault.auth.token/resolve-auth! after authenticating with a
+  ;; string token, should look this info up correctly on the token with a vault
+  ;; call
+  {:vault.auth/token (read-token)
+   :vault.auth/renewable? true
+   :vault.auth/expires-at (.plusSeconds (java.time.Instant/now) 60)})
 
 (defn start []
   (let [make-client (constantly (vault/new-client (vault-addr)))]
     (alter-var-root #'client (comp vault/start make-client))
-    (vault/authenticate! client (read-token))))
+    (vault/authenticate! client (read-token) #_ (read-auth))
+    ; ↓ enables the renewal behaviour by mutating the :auth key
+    (tok/resolve-auth! client)))
 
 (comment
   (start)
   (kv/list-secrets client "test_path")
+  (u/unveil (:auth client))
 
-  (def root-client (vault/new-client (vault-addr)))
-  (alter-var-root #'root-client vault/start)
-  (vault/authenticate! root-client "my_root_token")
+  (do
+   (def root-client (-> (vault-addr) vault/new-client vault/start))
+   (vault/authenticate! root-client "my_root_token")
+   ;; should never see this stream of #s dip below 45-50s remaining TTL
+   (dotimes [_i 60]
+     (Thread/sleep 1000)
+     (-> root-client
+         (tok/lookup-token {:token (:vault.auth/token (read-auth))})
+         :ttl
+         prn)))
 
-  (tok/lookup-token root-client {:token (read-token)})
-  (tok/renew-token! client {:token (read-token)}))
+  (tok/renew-token! root-client {:token (:vault.auth/token (read-auth))})
+
+  (vault/stop root-client)
+  (vault/stop client))
