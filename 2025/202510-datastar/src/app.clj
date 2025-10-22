97e793329c86a0bea2b5fcb224a47e1e3803ca80
Ryan McCuaig <ryan@ryanmccuaig.net>
2025-10-22 15:57:08 +0000

    Add reload, portal

--- 2025/202510-datastar/src/app.clj
+++ 2025/202510-datastar/src/app.clj
@@ -1,30 +1,37 @@
 (ns app
   (:require
-   [pogonos.core :as pg]
-   [reitit.ring :as rtr]
-   [ring.adapter.jetty :as jetty]
+   [pogonos.core                 :as pg]
+   [reitit.ring                  :as rtr]
+   [portal.api                   :as p]
+   [ring.adapter.jetty           :as jetty]
    [ring.middleware.content-type :refer [wrap-content-type]]
-   [ring.middleware.reload :refer [wrap-reload]]
-   [ring.middleware.resource :refer [wrap-resource]]))
+   [ring.middleware.reload       :refer [wrap-reload]]
+   [ring.middleware.resource     :refer [wrap-resource]]
+   [ring.middleware.session      :refer [wrap-session]]))
 
 (defonce *server (atom nil))
 
-(defn handler [_req]
+(defonce *portal (atom nil))
+
+(defn handler:home [_req]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (pg/render-resource "index.mustache.html" {})})
 
-(defn endpoint [_req]
-  {:status 200
-   :headers {"Content-Type" "text/html"
-             "datastar-selector" "#hal"
-             "datastar-mode" "inner"}
-   :body "<p>whatup</p>"})
+(defn handler:button [req]
+  (let [old (:session req)
+        new (update old :n (fnil inc 0))]
+    {:status 200
+     :headers {"Content-Type" "text/html"
+               "datastar-selector" "#hal"
+               "datastar-mode" "inner"}
+     :body (pg/render-string "<p>n {{ n }}</p>" {:n (:n new)})
+     :session new}))
 
 (def router
   (rtr/router
-   [["/" {:get #'handler}]
-    ["/endpoint" {:get #'endpoint}]]))
+   [["/" {:get #'handler:home}]
+    ["/endpoint" {:get #'handler:button}]]))
 
 (def app
   (-> (rtr/ring-handler
@@ -33,9 +40,18 @@
         {:not-found (constantly {:status 404})
          :method-not-allowed (constantly {:status 405})
          :not-acceptable (constantly {:status 406})}))
+      (wrap-session {:cookie-name "session"})
       (wrap-resource "public")
       (wrap-content-type)
       (wrap-reload)))
+
+(defn start-portal []
+  (add-tap #'p/submit)
+  (reset! *portal (p/open)))
+
+(defn stop-portal []
+  (p/close)
+  (remove-tap #'p/submit))
 
 (defn start []
   (reset! *server (jetty/run-jetty #'app {:port 3000 :join? false}))
@@ -51,4 +67,5 @@
   (start))
 
 (comment
-  (stop))
+  (stop)
+  (start-portal))
